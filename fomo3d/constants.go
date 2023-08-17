eda31dcdebcd9d020974071448674eb1eb3791b9
0xhackemost <0xhackermost@gmail.com>
2023-08-17 20:56:59 +0600

    feat: asylum v3 upgrade

--- fomo3d/constants.go
+++ fomo3d/constants.go
@@ -3,6 +3,6 @@ package fomo3d
 import "github.com/ethereum/go-ethereum/common"
 
 const FOMO3DV1 = "FOMO3DV1"
-const FOMO3DContractV1 = "0xEab90264100174dE865919085fF7f8fF88f730ca"
+const FOMO3DContractV1 = "0x99aD64B70c31ea08d191bb17370817A7CBcEfFd2"
 
 var FOMO3DContractV1Hex = common.HexToAddress(FOMO3DContractV1)
