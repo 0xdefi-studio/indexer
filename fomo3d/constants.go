087107956409b4f0b89935a53bf057f7db721749
0xhackemost <0xhackermost@gmail.com>
2023-08-17 11:51:22 +0600

    feat: v2 fomo3d deploy

--- fomo3d/constants.go
+++ fomo3d/constants.go
@@ -3,6 +3,6 @@ package fomo3d
 import "github.com/ethereum/go-ethereum/common"
 
 const FOMO3DV1 = "FOMO3DV1"
-const FOMO3DContractV1 = "0xaAb2DFBf2A7f1bbA3fd7E7a3BD06Bd50A0d45e2B"
+const FOMO3DContractV1 = "0xEab90264100174dE865919085fF7f8fF88f730ca"
 
 var FOMO3DContractV1Hex = common.HexToAddress(FOMO3DContractV1)
